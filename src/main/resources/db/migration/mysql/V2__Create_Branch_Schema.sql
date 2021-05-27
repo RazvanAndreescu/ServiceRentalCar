7bdb8851b56add518510b9c82ef8724b6bb68e93
Laurentiu Miu <joitaaurica@gmail.com>
2021-05-27 21:34:20 +0300

    LaurentiuMiu database attempt - drop unused schema

---
+++ src/main/resources/db/migration/mysql/V2__Create_Branch_Schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS branch
+(
+    branchlId INT PRIMARY KEY AUTO_INCREMENT,
+    address VARCHAR (30),
+
+    rentalId INT,
+
+    FOREIGN KEY(rentalId) REFERENCES rental(rentalId)
+
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
---
+++ src/main/resources/db/migration/mysql/V3__Create_Employee_Schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS employee
+(
+    employeeId INT PRIMARY KEY AUTO_INCREMENT,
+    nameEmployee VARCHAR (30),
+    roleEmployee VARCHAR (30),
+    branchId INT,
+
+    FOREIGN KEY(branchId) REFERENCES branch(branchId)
+
+    ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
