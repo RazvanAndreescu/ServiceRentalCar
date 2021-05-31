3c733387b57789c21a6a6ef5fd5896276d2c2f5e
Laurentiu Miu <joitaaurica@gmail.com>
2021-05-31 20:41:26 +0300

    customer verification 1.1

--- src/main/java/com/javaRemote/project/database/pop/CustomerPopulator.java
+++ src/main/java/com/javaRemote/project/database/pop/CustomerPopulator.java
@@ -1,10 +1,12 @@
-package com.javaRemote.project.database.populator;
+package com.javaRemote.project.database.pop;
 
 import com.javaRemote.project.database.entities.Customer;
 import com.javaRemote.project.database.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerPopulator implements CommandLineRunner {
 
     private final Customer customer = new Customer(
