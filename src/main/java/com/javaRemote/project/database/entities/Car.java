029593835ca379b9039d5f1f0ffad027d4651f88
Laurentiu Miu <joitaaurica@gmail.com>
2021-05-31 21:03:28 +0300

    customer verification 1.3

--- src/main/java/com/javaRemote/project/database/entities/Car.java
+++ src/main/java/com/javaRemote/project/database/entities/Car.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Data
@@ -37,9 +38,8 @@ public class Car {
     @JoinColumn(name="branchid")
     private Branch branch;
 
-    @ManyToOne
-    @JoinColumn(name="reservationid")
-    private Reservation reservation;
+    @OneToMany(mappedBy = "car")
+    private List<Reservation> reservations;
 
 
 }
