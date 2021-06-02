67bf121826963a6bd14f96211597806030a85c70
saboumariusbogdan <saboumariusbogdan.home@gmail.com>
2021-06-02 18:55:38 +0300

    Employee service - repository -- functionale / fara test

--- src/main/java/com/javaRemote/project/repository/EmployeeRepository.java
+++ src/main/java/com/javaRemote/project/repository/EmployeeRepository.java
@@ -11,5 +11,4 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     List<Employee> getEmployeeByNameEmployee(String nameEmployee);
     List<Employee> getEmployeeByRoleEmployee(String roleEmployee);
-
 }
