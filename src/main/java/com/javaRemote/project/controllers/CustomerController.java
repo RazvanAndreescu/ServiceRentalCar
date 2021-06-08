59a428904d5d2568292b1a20e3fb6b04c1a6b314
Laurentiu Miu <joitaaurica@gmail.com>
2021-06-08 15:02:58 +0300

    c, u, d controllers - updated services

--- src/main/java/com/javaRemote/project/controllers/CustomerController.java
+++ src/main/java/com/javaRemote/project/controllers/CustomerController.java
@@ -2,14 +2,12 @@ package com.javaRemote.project.controllers;
 
 import com.javaRemote.project.database.entities.Customer;
 import com.javaRemote.project.service.CustomerService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customers")
 public class CustomerController {
 
     private CustomerService customerService;
@@ -18,8 +16,34 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    @GetMapping("/listAll")
+    @GetMapping("/")
     public List<Customer> printAllCustomers(){
         return customerService.getAllCustomers();
     }
+
+    //Individual work
+    @PostMapping("/")
+    @ResponseBody
+    public Customer createCustomer(@RequestBody Customer customer){
+        customerService.createCustomer(customer);
+        return customer;
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public void updateCustomer(@PathVariable int id, @RequestBody Customer customer){
+        customerService.updateCustomerName(id, customer.getNameCustomer());
+        customerService.updateCustomerEmail(id, customer.getEmail());
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteCustomerById(@PathVariable int id){
+        try {
+            customerService.deleteCustomerById(id);
+        } catch (Exception e){
+            return false;
+        }
+        return customerService.getCustomerById(id) == null;
+    }
+
 }
