cd61a8c78ceeb700a296573302203ca3c5cb2b76
Laurentiu Miu <joitaaurica@gmail.com>
2021-06-08 15:02:58 +0300

    c, u, d controllers - updated services

--- src/main/java/com/javaRemote/project/service/CustomerService.java
+++ src/main/java/com/javaRemote/project/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.javaRemote.project.service;
 import com.javaRemote.project.database.entities.Customer;
 import com.javaRemote.project.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -10,65 +11,69 @@ public class CustomerService {
 
     private CustomerRepository customerRepository;
 
-    public CustomerService(CustomerRepository customerRepository){
+    public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
     // CREATE
-    public Customer createCustomer(Customer customer){
+    public Customer createCustomer(Customer customer) {
         return customerRepository.save(customer);
     }
 
     // READ
-    public List<Customer> getAllCustomers(){
+    public List<Customer> getAllCustomers() {
         return customerRepository.findAll();
     }
 
-    public List<Customer> getCustomersByName(String customerName){
+    public List<Customer> getCustomersByName(String customerName) {
         return customerRepository.findAllByNameCustomerContaining(customerName);
     }
 
-    public List<Customer> getCustomersByEmail(String email){
+    public List<Customer> getCustomersByEmail(String email) {
         return customerRepository.findAllByEmailContaining(email);
     }
 
-    public Customer getCustomerByEmail(String email){
+    public Customer getCustomerByEmail(String email) {
         return customerRepository.findAllByEmailContaining(email).get(0);
     }
 
-    public List<Customer> getCustomersByDrivingExperience(int drivingExperience){
+    public List<Customer> getCustomersByDrivingExperience(int drivingExperience) {
         return customerRepository.findAllByDrivingExperience(drivingExperience);
     }
 
-    public Customer getCustomerById(int customerId){
+    public Customer getCustomerById(int customerId) {
         return customerRepository.findCustomerByCustomerId(customerId);
     }
 
     // UPDATE
-    public Customer updateCustomerName(int id, String customerName){
+    public Customer updateCustomerName(int id, String customerName) {
         Customer customer = customerRepository.getById(id);
-        customer.setNameCustomer(customerName);
+        if (customerName != null) {
+            customer.setNameCustomer(customerName);
+        }
         return customerRepository.save(customer);
     }
 
-    public Customer updateCustomerEmail(int id, String customerEmail){
+    public Customer updateCustomerEmail(int id, String customerEmail) {
         Customer customer = customerRepository.getById(id);
-        customer.setEmail(customerEmail);
+        if (customerEmail != null) {
+            customer.setEmail(customerEmail);
+        }
         return customerRepository.save(customer);
     }
 
-    public Customer updateCustomerDrivingExperience(int id, int customerDrivingExperience){
+    public Customer updateCustomerDrivingExperience(int id, int customerDrivingExperience) {
         Customer customer = customerRepository.getById(id);
         customer.setDrivingExperience(customerDrivingExperience);
         return customerRepository.save(customer);
     }
 
     // DELETE
-    public void deleteCustomer(Customer customer){
+    public void deleteCustomer(Customer customer) {
         customerRepository.delete(customer);
     }
 
-    public void deleteCustomerById(int id){
+    public void deleteCustomerById(int id) {
         customerRepository.deleteById(id);
     }
 }
