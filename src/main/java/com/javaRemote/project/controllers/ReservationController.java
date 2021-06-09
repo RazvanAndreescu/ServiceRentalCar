e38878501db5d69c71b134fe41984a140117bc93
Laurentiu Miu <joitaaurica@gmail.com>
2021-06-09 15:39:26 +0300

    reservation controller

--- src/main/java/com/javaRemote/project/controllers/ReservationController.java
+++ src/main/java/com/javaRemote/project/controllers/ReservationController.java
@@ -2,14 +2,12 @@ package com.javaRemote.project.controllers;
 
 import com.javaRemote.project.database.entities.Reservation;
 import com.javaRemote.project.service.ReservationService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/reservation")
+@RequestMapping("/reservations")
 public class ReservationController {
 
     private ReservationService reservationService;
@@ -18,8 +16,35 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping("/listAll")
+    @GetMapping("/")
     public List<Reservation> printAllCustomers(){
         return reservationService.getAllReservation();
     }
+
+    @PostMapping("/")
+    @ResponseBody
+    public Reservation crateReservation(@RequestBody Reservation reservation){
+        return reservationService.create(reservation);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public boolean updateReservation(@PathVariable int id, @RequestBody Reservation reservation){
+        if(reservationService.getReservationById(id) != null){
+            reservationService.updateReservation(id, reservation);
+            return true;
+        }
+        return false;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public boolean deleteReservation(@PathVariable int id){
+        if(reservationService.getReservationById(id) != null){
+            reservationService.deleteReservationById(id);
+            return true;
+        }
+        return false;
+    }
+
 }
